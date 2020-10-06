d349e958ff3dae882711aa22767d0c03f41a6940
mnurmansah <mokhamadnurmansah@gmail.com>
2020-10-06 20:57:09 +0700

    Delete route register

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import Register from '../register/Register';
 import Login from '../login/Login';
 import Dashboard from '../dashboard/Dashboard';
 
@@ -14,7 +13,6 @@ function App() {
             <Switch>
               <Route path="/" exact><Dashboard/></Route>
               <Route path="/login" exact><Login/></Route>
-              <Route path="/register" exact><Register/></Route>
             </Switch>
           </Router>
         </Col>
