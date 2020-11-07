dad00f71fc54b929d85b8692068f35402b5b3fdc
mnurmansah <mokhamadnurmansah@gmail.com>
2020-11-07 22:24:09 +0700

    add login register forgotpassword saldoawal

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import Error404 from '../../components/error_404/Error404';
 import History from '../history/History';
 import Login from '../login/Login';
-import Create from '../create/Create';
-import Dashboard from '../dashboard/Dashboard';
-import Error404 from '../../components/error_404/Error404';
 import ForgotPassword from '../forgot_password/ForgotPassword';
+import Register from '../register/Register';
+import Dashboard from '../dashboard/Dashboard';
 import SaldoAwal from '../saldo_awal/SaldoAwal';
 
 const history = createBrowserHistory();
@@ -20,11 +20,11 @@ function App() {
           <Router history={history}>
             <Switch>
               <Route path="/" exact component={Dashboard} />
-              <Route path="/login" exact component={Login} />
-              <Route path="/create" exact component={Create} />
               <Route path="/history" exact component={History} />
-              <Route path="/forgotpassword" exact component={ForgotPassword} />
-              <Route path="/saldoawal" exact component={SaldoAwal} />
+              <Route path="/login" exact component={Login} />
+              <Route path="/register" exact component={Register} />
+              <Route path="/forgot-password" exact component={ForgotPassword} />
+              <Route path="/saldo-awal" exact component={SaldoAwal} />
               <Route path="*" component={Error404} />
             </Switch>
           </Router>
---
+++ src/containers/register/Register.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import { Button, FormGroup, FormControl} from "react-bootstrap";
+import "./Register.scss";
+
+export default function Register() {
+    const [email] = ("");
+    const [password] = ("");
+    const [cpassword] = ("");
+
+    return (<>
+     <div className="pd-0">
+      <form>
+      <p className="buat-akun"><img src="/images/texts/text_buat_akun.svg" alt="sort-icon"></img></p>
+        <FormGroup>
+          <FormControl
+            autoFocus
+            type="email"
+            value={email}
+            placeholder="Email"
+          />
+        </FormGroup>
+        <FormGroup controlId="password">
+          <FormControl
+            value={password}
+            placeholder="Kata Sandi"
+            type="password"
+          />
+        </FormGroup>
+        <FormGroup controlId="cpassword">
+          <FormControl
+            value={cpassword}
+            placeholder="Ulang Kata Sandi"
+            type="password"
+          />
+        </FormGroup>
+        <div class="form-group form-check">
+          <input type="checkbox" class="form-check-input"></input>
+          <p className="fs-13">
+          Lorem Ipsum is simply dummy text of the printing 
+          and typesetting industry. Lorem Ipsum has been 
+          the industry's standard dummy text ever since the 1500s
+          </p>
+        </div>
+        <Button className="custom-button mgb-10" block type="submit">
+        Masuk
+        </Button>
+      </form>
+      <p className="text-center fs-13">Sudah memiliki akun? <Link to="/login" > Masuk </Link></p>
+    </div>
+    </>
+  );
+}
